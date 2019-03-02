b42aa259695e947c3df452a72ae4ba727e666ba3
Hemang Rajyaguru <hemdagem@gmail.com>
2019-03-02 19:05:41 +0000

    added webpack compilation of button on a webpage

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Button } from "./Button";
+
+ReactDOM.render(
+    <Button />,
+    document.getElementById("example")
+);
